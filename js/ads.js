c870793ec2c49d943b388c6dafa20171b9757cb4
kayoz <dawid.kaj09@gmail.com>
2025-06-06 21:43:54 +0200

    feat: create ads.js

---
+++ js/ads.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var myModal = new bootstrap.Modal(document.getElementById('wideScreenModal'), {
+        keyboard: false
+    });
+    myModal.show();
+});
